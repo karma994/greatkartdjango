aa2fb16b87b757f49e1847e46218cee290b8e400
karma994 <ktenzleo@gmail.com>
2021-08-30 16:30:49 +0600

    registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,8 @@ from .models import Account
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password'
-        class: form-control,
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
         
         
     }))
@@ -25,3 +25,14 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password doesnot match!"
+            )
